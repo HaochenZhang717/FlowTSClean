de54c49d5a2c5d22bf7bc2e2f03dcea1d8cb5e56
Haochen Zhang <hz112@rice.edu>
2026-03-11 11:00:27 -0500

    0310

--- code/debug_causal_flow.py
+++ code/debug_causal_flow.py
@@ -1,3 +1,14 @@
+from typing import List, Tuple, Union
+from PIL import Image
+import numpy as np
+from collections import OrderedDict
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.distributed import DistributedSampler
+from torch.cuda.amp import GradScaler
+import os
+import json
+from collections import defaultdict
 from Trainers import DSPFlowTrainer
 from generation_models import CausalFlow
 
@@ -11,6 +22,94 @@ from tqdm import tqdm
 from torch.utils.data import TensorDataset, DataLoader
 
 
+
+def text2ts_collate_fn(batch):
+
+    ts = torch.stack([item["ts"] for item in batch], dim=0)                # (B, T, C)
+    text_embed = torch.stack([item["text_embed"] for item in batch], dim=0).mean(1) # (B, C, D)
+
+    batch_dict = {
+        "ts": ts,
+        "text_embed": text_embed,
+    }
+
+    return batch_dict
+
+
+class Text2TSDataset(Dataset):
+    """
+    Block-causal diffusion dataset.
+
+    原始:
+        self.ts: (N, T, C)
+
+    每个 __getitem__ 返回一个 (image, target_block) 的训练样本:
+        - ts:         (C, T)
+        - text_embed: (C, D)   # 只给 target block 的所有 channel text embedding
+        - loss_mask:  (T,)     # 只有 target block 对应区间为 1
+        - attn_mask: (T,)     # 只有 target block 对应区间为 1
+        - block_id:   int
+        - image_id:   str
+    """
+
+    def __init__(
+        self,
+        ts_path: str,
+        text_embed_path: str,
+    ):
+        self.ts = np.load(ts_path, allow_pickle=True)          # (N, T, C)
+        self.text_embed = torch.load(text_embed_path, map_location="cpu")
+
+
+        self.N, self.T, self.C = self.ts.shape
+
+        # image ids from embedding dict, e.g. ["image0", "image1", ...]
+        self.ids = sorted(
+            self.text_embed.keys(),
+            key=lambda x: int(x.replace("image", "")))
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+
+        image_id = self.ids[idx]
+        ts_id = int(image_id.replace("image", ""))
+
+        # ts: (T, C)
+        ts = self.ts[ts_id]
+
+        assert ts.shape[0] == self.T and ts.shape[1] == self.C
+
+        # 转成 (C, T)，更适合大多数 diffusion model
+        ts = torch.from_numpy(ts).float()  # (C, T)
+
+        # -----------------------------
+        # text condition:
+        # 只取 target block 的所有 channel embedding
+        # shape -> (C, D)
+        # -----------------------------
+        channel_embeds = []
+        for target_block in range(4):
+            for c in range(self.C):
+                key = f"seg{target_block + 1}_channel{c}"   # target_block 是 0-based, caption key 是 1-based
+                emb = self.text_embed[image_id][key]
+                channel_embeds.append(emb)
+
+        text_embed = torch.stack(channel_embeds, dim=0)   # (C, D)
+
+        # -----------------------------
+        # 构造 masks
+        # -----------------------------
+
+        sample = {
+            "ts": ts,                     # (C, T)
+            "text_embed": text_embed,     # (C, D)
+            "image_id": image_id,
+            "ts_id": ts_id,
+        }
+
+        return sample
 
 
 def save_args_to_jsonl(args, output_path):
@@ -36,8 +135,10 @@ def get_args():
     parser.add_argument("--n_heads", type=int, required=True)
 
     """data parameters"""
-    parser.add_argument("--train_data_path", type=str, required=True)
-    parser.add_argument("--valid_data_path", type=str, required=True)
+    parser.add_argument("--train_ts_path", type=str, required=True)
+    parser.add_argument("--train_embed_path", type=str, required=True)
+    parser.add_argument("--valid_ts_path", type=str, required=True)
+    parser.add_argument("--valid_embed_path", type=str, required=True)
 
     """training parameters"""
     parser.add_argument("--lr", type=float, required=True)
@@ -45,8 +146,6 @@ def get_args():
     parser.add_argument("--max_epochs", type=int, required=True)
     parser.add_argument("--grad_clip_norm", type=float, required=True)
     parser.add_argument("--grad_accum_steps", type=int, required=True)
-    parser.add_argument("--early_stop", type=str, required=True)
-    parser.add_argument("--patience", type=int, required=True)
 
     """wandb parameters"""
     parser.add_argument("--wandb_project", type=str,required=True)
@@ -60,7 +159,7 @@ def get_args():
 
 
 
-def unconditional_trian(args):
+def conditional_trian(args):
     os.makedirs(args.ckpt_dir, exist_ok=True)
     save_args_to_jsonl(args, f"{args.ckpt_dir}/config.jsonl")
 
@@ -74,24 +173,27 @@ def unconditional_trian(args):
         mlp_hidden_times=4,
     )
 
-    train_data = torch.from_numpy(np.load(args.train_data_path, allow_pickle=True))
-    train_set = TensorDataset(train_data)
+    train_set = Text2TSDataset(
+        args.train_ts_path,
+        args.train_embed_path,
+    )
 
-    valid_data = torch.from_numpy(np.load(args.valid_data_path, allow_pickle=True))
-    val_set = TensorDataset(valid_data)
+    val_set = Text2TSDataset(
+        args.train_ts_path,
+        args.train_embed_path,
+    )
 
 
     train_loader = torch.utils.data.DataLoader(
         train_set, batch_size=args.batch_size, num_workers=16,
-        shuffle=True, drop_last=True,
+        shuffle=True, drop_last=True, collate_fn=text2ts_collate_fn
     )
     val_loader = torch.utils.data.DataLoader(
         val_set, batch_size=args.batch_size, num_workers=16,
-        shuffle=False, drop_last=False,
+        shuffle=False, drop_last=False, collate_fn=text2ts_collate_fn
     )
 
     optimizer= torch.optim.Adam(model.parameters(), lr=args.lr)
-
 
     device = torch.device(f"cuda:{args.gpu_id}" if torch.cuda.is_available() else "cpu")
     trainer = DSPFlowTrainer(
@@ -106,17 +208,16 @@ def unconditional_trian(args):
         wandb_project_name=args.wandb_project,
         grad_clip_norm=args.grad_clip_norm,
         grad_accum_steps=args.grad_accum_steps,
-        compile=False,
     )
 
-    trainer.uncond_train(config=vars(args))
+    trainer.cond_train(config=vars(args))
 
 
 
 def main():
     args = get_args()
 
-    unconditional_trian(args)
+    conditional_trian(args)
 
 
 
