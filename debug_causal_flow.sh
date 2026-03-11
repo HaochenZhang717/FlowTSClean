ac3e5631310909e553153f24259e277b8593d9d5
Haochen Zhang <hz112@rice.edu>
2026-03-11 10:12:49 -0500

    0310

---
+++ debug_causal_flow.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# =============================
+# GPU
+# =============================
+export CUDA_VISIBLE_DEVICES=2
+export hucfg_t_sampling="logitnorm"
+export hucfg_num_steps="100"
+
+
+
+
+# =============================
+# Paths
+# =============================
+TRAIN_DATA="/playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u/train_ts.npy"
+VALID_DATA="/playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u/valid_ts.npy"
+CKPT_DIR="/playpen/haochenz/FlowTS/unconditional/synth_u_0310"
+
+
+# =============================
+# WandB
+# =============================
+WANDB_PROJECT="FlowTS"
+WANDB_RUN="synth_u"
+
+# =============================
+# Run training
+# =============================
+python debug_causal_flow.py \
+    --seq_len 128 \
+    --feature_size 1 \
+    \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 128 \
+    --n_heads 8 \
+    \
+    --train_data_path ${TRAIN_DATA} \
+    --valid_data_path ${VALID_DATA} \
+    \
+    --lr 1e-4 \
+    --batch_size 64 \
+    --max_epochs 1000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "True" \
+    --patience 20 \
+    \
+    --wandb_project $WANDB_PROJECT \
+    --wandb_run $WANDB_RUN \
+    \
+    --gpu_id 0 \
+    \
+    --ckpt_dir ${CKPT_DIR}
